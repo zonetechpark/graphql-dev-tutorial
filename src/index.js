96d8f10379cbb6b3813f9b76bb5cf2e311d0f777
Daniel Ale <danielale9291@gmail.com>
2020-02-11 14:40:27 +0100

    Class 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 // const typeDefs = require("./types/schema");
 const resolvers = require("./resolvers");
 
+async function main() {
+  const newUser = await prisma.createUser({
+    name: "John",
+    age: 34
+  });
+  console.log(newUser);
+
+  const allUser = await prisma.users();
+  console.log(allUser);
+}
+
 const server = new GraphQLServer({
   typeDefs: "./src/types/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
+// main().catch(err => console.error(err));
 server.start(() => console.log("Server started on localhost:4000"));
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,27 +1,8 @@
-let users = [
-  {
-    id: "user-0",
-    name: "John",
-    age: 20
-  }
-];
-
-let userCount = users.length;
-
 const resolvers = {
   Query: {
     info: () => `Hello World`,
-    users: () => users
-  },
-  Mutation: {
-    createUser: (parent, { name, age }) => {
-      const user = {
-        id: `user-${userCount++}`,
-        name,
-        age
-      };
-      users.push(user);
-      return user;
+    users: (parent, args, context, info) => {
+      return context.prisma.users();
     }
   }
 };
